07b5bcb319fa5bad8b6c9742ea9bf1530dbb04a0
seewes <doobix@gmail.com>
2014-07-24 15:14:48 -0700

    Finished prototypal and pseudoclassical.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.head = 0;
+  this.tail = 0;
+  this.storage = {};
 };
 
-
+Queue.prototype.size = function() {
+  return this.tail - this.head;
+}
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.tail] = value;
+  this.tail++;
+}
+Queue.prototype.dequeue = function() {
+  var result = this.storage[this.head];
+  if (this.tail - this.head > 0) {
+    delete this.storage[this.head];
+    this.head++;
+  }
+  return result;
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,9 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  this.size = 0;
+  this.totalSize = 0;
   this.storage = {};
 };
 Stack.prototype.size = function() {
-  return this.size;
+  return this.totalSize;
+}
+Stack.prototype.push = function(value) {
+  this.storage[this.totalSize] = value;
+  this.totalSize++;
+}
+Stack.prototype.pop = function() {
+  if (this.totalSize > 0) {
+    this.totalSize--;
+  }
+  var result = this.storage[this.totalSize];
+  delete this.storage[this.totalSize];
+  return result;
 }
