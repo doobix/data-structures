a743a01b7aa7cde2e736deb41a9cfde3545411d3
seewes <doobix@gmail.com>
2014-07-24 20:13:38 -0700

    Finished Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var tempNode = makeNode(value);
+    if (list.head === null) {
+      list.head = tempNode;
+    }
+    if (list.tail !== null) {
+      list.tail.next = tempNode;
+    }
+    list.tail = tempNode;
   };
 
   list.removeHead = function(){
+    var tempHead = list.head.value;
+    list.head = list.head.next;
+    return tempHead;
   };
 
   list.contains = function(target){
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      if (current.next === null) {
+        break;
+      } else {
+        // current.next = current.next.next;
+        current = current.next;
+      }
+    }
+    return false;
   };
 
   return list;
